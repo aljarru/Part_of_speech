75eb7e7b153fdd265af8ca99ff4d179c6ce8f5a3
irinahumainr <irina@humainr.com>
2022-11-23 10:17:07 +0100

    start of project

---
+++ pymorphy2.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 23 09:09:18 2022
+
+@author: irina
+"""
+import streamlit as st
+import pymorphy2 as pm
+import pymorphy2_dicts_ru as ru
+
+
+text='Повесть эта о том, что случилось, когда твой дедушка был еще маленьким. Ее очень важно прочесть, чтобы понять, как возникла связь между нашим миром и Нарнией.'
+
+st.title("Разбор предложения по частям речи")
+st.write(text)
