80f8fc64d8dd601fb2198c6b5ba21cd02f8a3b4b
aljarru <irina@wolthers.biz>
2022-12-01 18:05:26 +0100

    Update pymorphy2.py

--- pymorphy2.py
+++ pymorphy2.py
@@ -4,13 +4,25 @@ Created on Wed Nov 23 09:09:18 2022
 
 @author: irina
 """
+
 import streamlit as st
-import pymorphy2 as pm
-import pymorphy2_dicts_ru as ru
+import re
+import nltk
+from nltk.tag import pos_tag
+from nltk.tokenize import word_tokenize
 
 
-text='Повесть эта о том, что случилось, когда твой дедушка был еще маленьким. Ее очень важно прочесть, чтобы понять, как возникла связь между нашим миром и Нарнией.'
+
+text="This story is about something that happened to them when they were sent away from London during the war because of the air-raids."
 
 st.title("Разбор предложения по частям речи")
 st.write(text)
-st.write(text)
+text_clean=re.sub(r'[.,"\'-?:!;]', '', text)
+text_lower=text_clean.lower()
+st.write(text_lower)
+li=list(text_lower.split(" "))
+st.write(li)
+text_tokens = word_tokenize(text)
+
+text_tagged = pos_tag(li)
+st.write(text_tagged)
