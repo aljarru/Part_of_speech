6fa29db2e00a93f5739c30f53fe22c388bc91f32
aljarru <irina@wolthers.biz>
2022-12-03 10:42:53 +0100

    Update nltk_sent.py

--- nltk_sent.py
+++ nltk_sent.py
@@ -1,10 +1,8 @@
 import streamlit as st
 import re
-
-text_tokens =nltk.pos_tag(nltk.word_tokenize(text))
-print(text_tokens)
-st.write(text_tokens
 import nltk
+
+
 
 text="This story is about something that happened to them when they were sent away from London during the war because of the air-raids."
 
@@ -12,5 +10,14 @@ st.title("Разбор предложения по частям речи")
 st.write(text)
 
 text_tokens =nltk.pos_tag(nltk.word_tokenize(text))
-print(text_tokens)
-st.write(text_tokens
+
+st.write("Удаляем существительные из текста")
+text_new=''
+for i in text_tokens:
+    print(i[1])
+    if i[1]=='NN':
+        text_new+='___________'
+    else:
+        text_new+= ' '+ i[0] + ' '
+print(text_new)
+st.write(text_new)
